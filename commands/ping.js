28287859f1103f5024083691d4308defd1fb01bc
AirFusion <jfang.cv.ca.us@gmail.com>
2021-01-20 14:26:39 -0800

    update

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-exports.run = function (client, message) {
-  var guild = message.guild;
-  message.channel.send(':ping_pong: Pinging...').then((msg) => {
-    msg.edit(`:ping_pong: Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-  });
-  client.logger.log('info', `ping command used by ${message.author.username} Time: ${Date()} Guild: ${guild}`)
-}
--- events/disconnect.js
+++
@@ -1,4 +0,0 @@
-const chalk = require('chalk');
-module.exports = client => {
-    console.log(chalk.bold("You have been disconnected @ " + Date()));
-}
